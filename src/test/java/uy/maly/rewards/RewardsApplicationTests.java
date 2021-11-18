5ba191a628854679fcb71b3fa4ec2b3df28f128e
Maly Miraballes, Javier Andres <jmaly@antel.com.uy>
2021-11-18 19:03:17 -0300

    Initial commit

---
+++ src/test/java/uy/maly/rewards/RewardsApplicationTests.java
@@ -0,0 +1,13 @@
+package uy.maly.rewards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
